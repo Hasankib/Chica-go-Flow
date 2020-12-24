e08d5232612796aec45a15bc6f74090868b33b17
kibriah <kibriah@mcmaster.ca>
2020-12-24 02:05:43 -0500

    new bare bones working app

--- api/api.py
+++ api/api.py
@@ -1,8 +1,23 @@
 import time
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
+@app.route('/login', methods=['POST'])
+def login():
+    login_json = request.get_json()
+
+    if not login_json:
+        return jsonify({'msg': 'Missing JSON'}), 400
+
+    username = login_json.get('username')
+    password = login_json.get('password')
+    print(username)
+    print(password)
+
+    return jsonify({'username': username}), 200
+
--- src/App.js
+++ src/App.js
@@ -4,12 +4,29 @@ import './App.css';
 
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
+  const [username, setUsername] = useState("ajhd");
+  const [password, setPassword] = useState("ajhd");
 
   useEffect(() => {
     fetch('/time').then(res => res.json()).then(data => {
       setCurrentTime(data.time);
     });
   }, []);
+
+  const handleLogin = e => {
+    e.preventDefault();
+    fetch('/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password
+      })
+    })
+      .then(response => response.json())
+  };
 
   return (
     <div className="App">
@@ -19,8 +36,76 @@ function App() {
 
         <p>The current time is {currentTime}.</p>
       </header>
+      <form>
+          Username:
+          <br />
+          <input
+            type="text"
+            name="username"
+            value={username}
+          />
+          <br />
+          Password:
+          <br />
+          <input
+            type="password"
+            name="password"
+            value={password}
+          />
+          <br />
+          <button onClick={this.handleLogin()}>Login</button>
+        </form>
     </div>
   );
 }
 
+/*class Login extends React.Component {
+  state = {
+    username: 'fjdh',
+    password: 'dhihf'
+  };
+  
+  
+    handleLogin = e => {
+      e.preventDefault();
+      fetch('/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          username: this.state.username,
+          password: this.state.password
+        })
+      })
+        .then(response => response.json())
+    };
+  
+  
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          Username:
+          <br />
+          <input
+            type="text"
+            name="username"
+            value={this.state.username}
+          />
+          <br />
+          Password:
+          <br />
+          <input
+            type="password"
+            name="password"
+            value={this.state.password}
+          />
+          <br />
+          <button onClick={this.handleLogin}>Login</button>
+        </form>
+      );
+    }
+  }*/
+
 export default App;
+//export default Login;
